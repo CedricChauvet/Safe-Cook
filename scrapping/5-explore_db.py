c0824693089ed7fb70e2c71f7dedb45370ac9b49
CedricChauvet <chauvetc.ujf@gmail.com>
2025-01-01 17:36:07 +0100

    improving  database with a 10 elements db called recipes_mix1

--- scrapping/5-explore_db.py
+++ scrapping/5-explore_db.py
@@ -1,10 +1,13 @@
+"""
+afin de voir si la base de données est bien remplie, on va afficher les recettes
+recipes_mix1 est la collection de la base de données 0safe-cook de 10 recettes
+"""
 from pymongo import MongoClient
 
-from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['0safe-cook']  # Nom de la base de données
-recipes = db['recipes_noel']  # Nom de la collection
+recipes = db['recipes_mix1']  # Nom de la collection
 
 # Correction ici : pas besoin de .find() sur recipe_list
 for recipe in recipes.find():
