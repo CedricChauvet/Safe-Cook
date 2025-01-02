5d99db0138266f127e9f7403aa8fea63bef0f162
CedricChauvet <chauvetc.ujf@gmail.com>
2025-01-02 11:37:29 +0100

    finishing scrapping

--- scrapping/5-explore_db.py
+++ scrapping/5-explore_db.py
@@ -1,14 +1,20 @@
 """
-afin de voir si la base de données est bien remplie, on va afficher les recettes
+afin de voir si la base de données est bien remplie,
+on va afficher les recettes
 recipes_mix1 est la collection de la base de données 0safe-cook de 10 recettes
 """
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['0safe-cook']  # Nom de la base de données
-recipes = db['recipes_mix1']  # Nom de la collection
+def main():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['0safe-cook']  # Nom de la base de données
+    recipes = db['recipes_mix1']  # Nom de la collection
 
-# Correction ici : pas besoin de .find() sur recipe_list
-for recipe in recipes.find():
-    print(recipe["title"])
+    # Correction ici : pas besoin de .find() sur recipe_list
+    for recipe in recipes.find():
+        print(recipe["title"])
+
+
+if __name__ == "__main__":
+    main()
