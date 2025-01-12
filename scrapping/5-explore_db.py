d2dea305b3d982482895d9d44f145ef108e1d5ac
CedricChauvet <chauvetc.ujf@gmail.com>
2025-01-12 16:06:31 +0100

    push dimanche avec reic

--- scrapping/5-explore_db.py
+++ scrapping/5-explore_db.py
@@ -9,7 +9,7 @@ from pymongo import MongoClient
 def main():
     uri = "mongodb+srv://9184:f9XGDwYrIBnUnNkw@cluster0.ufblf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Créer une instance de client
-    client = MongoClient("htpps://localhost:27017")
+    client = MongoClient(uri)
     db = client['0safe-cook']  # Nom de la base de données
     #recipes = db['recipes_mix1']  # Nom de la collection
     recipes = db['recipes_many_txt']  # Nom de la collection
