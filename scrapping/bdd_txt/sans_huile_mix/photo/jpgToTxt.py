0b95d6fe5e8b4a7ea096210a3a248ad7178ea5dc
CedricChauvet <chauvetc.ujf@gmail.com>
2025-01-15 10:31:26 +0100

    création des fichier en b64

---
+++ scrapping/bdd_txt/sans_huile_mix/photo/jpgToTxt.py
@@ -0,0 +1,23 @@
+
+import os
+import glob
+import base64
+
+
+# Spécifiez le chemin du répertoire
+path = './'
+
+# Utilisez glob pour trouver tous les fichiers .txt
+fichiers_txt = glob.glob(os.path.join(path, '*.jpg'))
+liste_recettes = [] 
+for file in fichiers_txt:
+    print()
+
+    # Lire l'image JPG
+    with open(file, 'rb') as image_file:
+        # Convertir en base64
+        encoded_string = base64.b64encode(image_file.read())
+        
+    # Enregistrer le base64 dans un fichier texte
+    with open(f'{file[:-4]}.txt', 'wb') as txt_file:
+        txt_file.write(encoded_string)
